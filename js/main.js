25c2893936177eaac25a929788f80612ae333c84
xita Rps <xitarps@gmail.com>
2020-11-11 20:57:04 -0300

    Add top and bottom restrictions

--- js/main.js
+++ js/main.js
@@ -51,19 +51,29 @@ function start() { // Inicio da função start()
 	
   } // fim da função movefundo()
   
-  
+
   function movejogador() {
 	
     if (jogo.pressionou[TECLA.W]) {
       var topo = parseInt($("#jogador").css("top"));
       $("#jogador").css("top",topo-10);
-    
+
+      if (topo<=0) {
+		
+        $("#jogador").css("top",topo+10);
+      }
+      
     }
     
     if (jogo.pressionou[TECLA.S]) {
       
       var topo = parseInt($("#jogador").css("top"));
       $("#jogador").css("top",topo+10);	
+
+      if (topo>=434) {	
+        $("#jogador").css("top",topo-10);
+          
+      }
     }
     
     if (jogo.pressionou[TECLA.D]) {
