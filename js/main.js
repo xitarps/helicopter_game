d0b5c0a7f65f1f46b92c07dbf7a6267808cfb03f
xita Rps <xitarps@gmail.com>
2020-11-11 20:54:05 -0300

    Add player move (W,S)

--- js/main.js
+++ js/main.js
@@ -9,7 +9,25 @@ function start() { // Inicio da função start()
   
   //Principais variáveis do jogo
 	
-	var jogo = {}
+  var jogo = {}
+  var TECLA = {
+    W: 87,
+    S: 83,
+    D: 68
+  }
+  
+  jogo.pressionou = [];
+
+  //Verifica se o usuário pressionou alguma tecla	
+	
+	$(document).keydown(function(e){
+    jogo.pressionou[e.which] = true;
+  });
+  
+  
+  $(document).keyup(function(e){
+    jogo.pressionou[e.which] = false;
+  });
 	
 	//Game Loop
 
@@ -17,7 +35,8 @@ function start() { // Inicio da função start()
 	
 	function loop() {
 	
-	movefundo();
+  movefundo();
+  movejogador();
 	
 	} // Fim da função loop()
 
@@ -27,9 +46,31 @@ function start() { // Inicio da função start()
 	
 	function movefundo() {
 	
-	esquerda = parseInt($("#fundoGame").css("background-position"));
-	$("#fundoGame").css("background-position",esquerda-1);
+    esquerda = parseInt($("#fundoGame").css("background-position"));
+    $("#fundoGame").css("background-position",esquerda-1);
 	
-	} // fim da função movefundo()
+  } // fim da função movefundo()
+  
+  
+  function movejogador() {
+	
+    if (jogo.pressionou[TECLA.W]) {
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top",topo-10);
+    
+    }
+    
+    if (jogo.pressionou[TECLA.S]) {
+      
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top",topo+10);	
+    }
+    
+    if (jogo.pressionou[TECLA.D]) {
+      
+      //Chama função Disparo	
+    }
+
+  } // fim da função movejogador()
 
 } // Fim da função start
