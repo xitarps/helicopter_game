340d174efc548b091a2f824a416396eb5eb0ffef
xita Rps <xitarps@gmail.com>
2020-11-11 21:02:24 -0300

    Add enemy moves

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ function start() { // Inicio da função start()
   //Principais variáveis do jogo
 	
   var jogo = {}
+
   var TECLA = {
     W: 87,
     S: 83,
@@ -17,6 +18,9 @@ function start() { // Inicio da função start()
   }
   
   jogo.pressionou = [];
+
+  var velocidade=5;
+  var posicaoY = parseInt(Math.random() * 334);
 
   //Verifica se o usuário pressionou alguma tecla	
 	
@@ -37,6 +41,7 @@ function start() { // Inicio da função start()
 	
   movefundo();
   movejogador();
+  moveinimigo1();
 	
 	} // Fim da função loop()
 
@@ -82,5 +87,22 @@ function start() { // Inicio da função start()
     }
 
   } // fim da função movejogador()
+
+
+  function moveinimigo1() {
+
+    posicaoX = parseInt($("#inimigo1").css("left"));
+    $("#inimigo1").css("left",posicaoX-velocidade);
+    $("#inimigo1").css("top",posicaoY);
+      
+      if (posicaoX<=0) {
+        posicaoY = parseInt(Math.random() * 334);
+        $("#inimigo1").css("left",694);
+        $("#inimigo1").css("top",posicaoY);
+      }
+  } //Fim da função moveinimigo1()
+
+
+
 
 } // Fim da função start
