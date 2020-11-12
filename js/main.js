7478dafd5b1d77ad6c54d036b036f61501f88f30
xita Rps <xitarps@gmail.com>
2020-11-11 21:16:21 -0300

    Add shot

--- js/main.js
+++ js/main.js
@@ -22,6 +22,8 @@ function start() { // Inicio da função start()
   var velocidade=5;
   var posicaoY = parseInt(Math.random() * 334);
 
+  var podeAtirar=true;
+
   //Verifica se o usuário pressionou alguma tecla	
 	
 	$(document).keydown(function(e){
@@ -85,7 +87,8 @@ function start() { // Inicio da função start()
     
     if (jogo.pressionou[TECLA.D]) {
       
-      //Chama função Disparo	
+      //Chama função Disparo
+      disparo();
     }
 
   } // fim da função movejogador()
@@ -129,6 +132,39 @@ function start() { // Inicio da função start()
   
   } // fim da função moveamigo()
 
+
+  function disparo() {
+	
+    if (podeAtirar==true) {
+      
+      podeAtirar=false;
+      
+      topo = parseInt($("#jogador").css("top"))
+      posicaoX= parseInt($("#jogador").css("left"))
+      tiroX = posicaoX + 190;
+      topoTiro=topo+37;
+      $("#fundoGame").append("<div id='disparo'></div");
+      $("#disparo").css("top",topoTiro);
+      $("#disparo").css("left",tiroX);
+      
+      var tempoDisparo=window.setInterval(executaDisparo, 30);
+    
+    } //Fecha podeAtirar
+   
+    function executaDisparo() {
+      posicaoX = parseInt($("#disparo").css("left"));
+      $("#disparo").css("left",posicaoX+15); 
+  
+      if (posicaoX>900) {
+              
+        window.clearInterval(tempoDisparo);
+        tempoDisparo=null;
+        $("#disparo").remove();
+        podeAtirar=true;
+            
+      }
+    } // Fecha executaDisparo()
+  } // Fecha disparo()
 
 
 } // Fim da função start
