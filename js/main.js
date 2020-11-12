3318bd246fbf42e5b33fdab56698ca025c46a8ce
xita Rps <xitarps@gmail.com>
2020-11-11 23:10:39 -0300

    Add change difficulty

--- js/main.js
+++ js/main.js
@@ -217,6 +217,7 @@ function start() { // Inicio da função start()
         
     if (colisao3.length>0) {
       pontos=pontos+100;
+      velocidade=velocidade+0.3;
       inimigo1X = parseInt($("#inimigo1").css("left"));
       inimigo1Y = parseInt($("#inimigo1").css("top"));
         
